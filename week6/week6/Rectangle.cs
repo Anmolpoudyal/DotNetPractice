0d583dd1bc77e40e7dbf339e3bbf8c2f2bb8d2fd
Anmol_poudyal <137183235+Anmolpoudyal@users.noreply.github.com>
2025-12-01 11:29:16 +0545

    delegates practice

---
+++ week6/week6/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week6
+{
+  public class Rectangle
+  {
+    private double length,breadth;
+
+        // creating getter and setter using expression body
+
+        public double Length
+        {
+            get => length;
+            set => length = value;
+        }
+        public double Breadth
+        {
+            get => breadth;
+            set => breadth = value;
+        }
+        public double Area() => length * breadth;
+
+        public string ShowDetails() => $"Length: {length}, Breadth: {breadth}, Area: {Area()}";
+        
+
+
+
+
+    }
+}
